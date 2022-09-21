1cad032c8dae859c566f3ffb28b191e5530c17d9
mynameisanv <kandianvesh@gmail.com>
2022-09-20 22:14:16 -0500

    Add slider, refactor code

--- script.js
+++ script.js
@@ -1,64 +1,46 @@
 const box = document.querySelector('.box');
 const randomButton = document.getElementById('randomBtn');
-const selButton = document.getElementById('sortStartBtn');
-const bblButton = document.querySelector('.bblBtn');
+const sortButton = document.getElementById('sortStartBtn');
 const dropMenuButton = document.getElementById('sortSelBtn');
+const bblButton = document.querySelector('.bblBtn');
 const dropMenu = document.querySelector('.dropdown-menu');
 const dropDownOptions = document.querySelectorAll('.dropdown-option');
 const RED = "element-red";
 const BLUE = "element-blue";
 const GREEN = "element-green";
 
+var slide = document.querySelector('.slider'), 
+    sliderVal = document.querySelector('.slider-val');
 
-let barCount = 100;
-let BAR_SIZE = 100 / barCount;
+
+let barCount = slide.value;
+let barSize = 100 / barCount;
 let delay = 100;
 let currSortOption = '';
 var elements = [];
 
+window.addEventListener('load', ()=>{
+    genBars(barCount);
+})
 
-// Generates original set sorted bars
-for (var i = 0; i<barCount; i++){
-    const div = document.createElement('div');
-    div.className = "element";
-    div.style.height = (i+1)*BAR_SIZE + "%";
-    div.style.width = BAR_SIZE + "%"
-    div.style.left = i*BAR_SIZE + "%";
-    elements.push(div);
-    box.appendChild(div);
-}
-
-// Randomizes bars
-async function randomize(bars){
-    for (var i = bars.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        await swap(i,j, 10);
-    }
-}
-
-// Swaps two values in an array, paints swapped bar red
-async function swap (i,j, delay){
-    elements[i].classList.add(RED);
-    [elements[i].style.left, elements[j].style.left] = [elements[j].style.left, elements[i].style.left];
-    [elements[i],elements[j]] = [elements[j],elements[i]];
-    await sleep(delay);
-    elements[j].classList.remove(RED);
-}
-
-function sleep(delay) {
-    return new Promise(resolve => {
-        setTimeout(resolve, delay);
-    });
-}
 
 randomButton.addEventListener('click', ()=> {
     randomize(elements);
 })
-// bblButton.addEventListener('click', ()=> {
-//     bblSort(elements);
-// })
-selButton.addEventListener('click', ()=> {
-    selSort(elements);
+
+sortButton.addEventListener('click', ()=>{
+    switch(currSortOption){
+    case 'Bubble Sort':
+        bblSort(elements);
+        break;
+    case 'Selection Sort':
+        selSort(elements);
+        break;
+    default:
+        dropMenuButton.classList.add('error');
+        break;
+    }
+    
 })
 
 // Handles sort menu drop down
@@ -84,12 +66,65 @@ document.addEventListener('click', e => {
 // Allows user to select a sort from drop down menu
 // When user selects option, displays the current sort option
 for (let i=0; i < dropDownOptions.length; i++){
-    dropDownOptions[i].addEventListener('click', () => {
-        dropMenuButton.innerHTML = dropDownOptions[i].innerHTML;
-        currSortOption = dropDownOptions[i].innerHTML;
-        console.log(currSortOption);
-        dropMenu.classList.toggle('active');
+        dropDownOptions[i].addEventListener('click', () => {
+            if(dropMenuButton.classList.contains('error')) dropMenuButton.classList.remove('error');
+
+            dropMenuButton.innerHTML = dropDownOptions[i].innerHTML;
+            currSortOption = dropDownOptions[i].innerHTML;
+            dropMenu.classList.toggle('active');
     })
+}
+
+slide.oninput = function() {
+    sliderVal.innerHTML = "Elements: " + this.value;
+    genBars(this.value);
+}
+
+// Generates original set sorted bars
+function genBars(barCount){
+    clearBox(box);
+    barSize = 100 / barCount;
+    elements = [];
+
+    for (var i = 0; i<barCount; i++){
+        const div = document.createElement('div');
+        div.className = "element";
+        div.style.height = (i+1)*barSize + "%";
+        div.style.width = barSize + "%"
+        div.style.left = i*barSize + "%";
+        elements.push(div);
+        box.appendChild(div);
+    }
+}
+
+function clearBox(box) {
+    while (box.lastElementChild) {
+        elements.pop();
+        box.removeChild(box.lastElementChild);
+    }
+}
+
+// Randomizes bars
+async function randomize(bars){
+    for (var i = bars.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        await swap(i,j, 10);
+    }
+}
+
+// Swaps two values in an array, paints swapped bar red
+async function swap (i,j, delay){
+    elements[i].classList.add(RED);
+    [elements[i].style.left, elements[j].style.left] = [elements[j].style.left, elements[i].style.left];
+    [elements[i],elements[j]] = [elements[j],elements[i]];
+    await sleep(delay);
+    elements[j].classList.remove(RED);
+}
+
+function sleep(delay) {
+    return new Promise(resolve => {
+        setTimeout(resolve, delay);
+    });
 }
 
 //////////////////////////////////////////
@@ -98,6 +133,7 @@ for (let i=0; i < dropDownOptions.length; i++){
 
 // BUBBLE SORT
 async function bblSort(bars){
+    delay = 5000/bars.length;
     for(var i = 1; i < bars.length; i++){
       for(var j = 0; j < ( bars.length - i); j++){
         if(bars[j].offsetHeight > bars[j+1].offsetHeight){
@@ -109,6 +145,7 @@ async function bblSort(bars){
 
 // SELECTION SORT
 async function selSort(bars){
+    delay = 5000/bars.length;
     for (let i = 0; i < bars.length; i++){
         let min = i;
         for (let j = i+1; j < bars.length; j++){
